7d50d93e2bb16a03b4fa3755044e7daff26d3805
rozanzakyy <rozanzzzaky@gmail.com>
2026-02-24 23:52:41 +0700

    ubah nama desa

--- resources/views/frontend/layout.blade.php
+++ resources/views/frontend/layout.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Website Resmi Kampung Badran Sari">
+    <meta name="description" content="Website Resmi Kampung Kedaton Sari">
     <title>@yield('title', 'Beranda') - Kampung Badran Sari</title>
     
     <!-- Favicon -->
